449d69f2fd73b1c2deaf8c02495dc79684451eb1
bonane <ndayishimyebonane@gmail.com>
2024-04-08 21:56:07 +0200

    customizing dashboard

--- routes/web.php
+++ routes/web.php
@@ -32,11 +32,16 @@ Route::middleware('auth')->group(function () {
 
 
 require __DIR__ . '/auth.php';
-
+/////// Admin Group Middleware
 Route::middleware('auth', 'roles:admin')->group(function () {
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
-});
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+}); // End Admin Group Middleware
 
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+
+////// Instructor Group Middleware
 Route::middleware('auth', 'roles:instructor')->group(function () {
     Route::get('/instructor/dashboard', [InstructorController::class, 'InstructorDashboard'])->name('instructor.dashboard');
-});
+}); // End instructor Group Middleware
